3584bd32fbc86c5c3c3305d7636dd4e0b3474e61
Daniel McAteer <danielmcateer@Daniels-MacBook-Pro.local>
2024-03-08 19:53:54 -0500

    concatenate all Obsidian

---
+++ concat_all_obsidian.py
@@ -0,0 +1,22 @@
+import os
+
+def traverse_vault(vault_path, output_file):
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for root, dirs, files in os.walk(vault_path):
+            for file in files:
+                if file.endswith('.md') or file.endswith('.txt'):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r', encoding='utf-8') as infile:
+                        content = infile.read()
+                        outfile.write(content + '\n\n')
+
+# Specify the path to your Obsidian MD vault
+vault_path = '/Users/danielmcateer/Library/Mobile Documents/iCloud~md~obsidian/Documents/Ideaverse'
+
+# Specify the output file name
+output_file = 'all_dm_notes.txt'
+
+# Call the function to traverse the vault and write the data
+traverse_vault(vault_path, output_file)
+
+print(f"Data from the Obsidian MD vault has been written to {output_file}.")
